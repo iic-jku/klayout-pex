07855a669f8e989adac96322afc71f1940e6c152
Martin Köhler <info@martinjankoehler.com>
2024-12-02 17:34:34 +0100

    Renamed and rewrote CLI program, added __main__.py

--- kpex/kpex_cli.py
+++ kpex/kpex_cli.py
@@ -1,17 +1,25 @@
 #! /usr/bin/env python3
 
 import argparse
+import os
 import os.path
 import sys
 
-import klayout.db as kdb
-
-from fastercap.fastercap_file_writer import *
-from fastercap.fastercap_input_builder import FasterCapInputBuilder
-
-from klayout.lvsdb_extractor import KLayoutExtractionContext
+from .fastercap.fastercap_file_writer import *
+from .fastercap.fastercap_input_builder import FasterCapInputBuilder
+from .klayout.lvsdb_extractor import KLayoutExtractionContext
+from .logging import (
+    # console,
+    # debug,
+    # info,
+    # warning,
+    error
+)
 from .tech_info import TechInfo
 from .version import __version__
+
+import klayout.db as kdb
+
 
 # ------------------------------------------------------------------------------------
 
@@ -28,12 +36,12 @@ def parse_args(arg_list: List[str] = None) -> argparse.Namespace:
     group_special.add_argument("--version", "-v", action='version', version=f'{PROGRAM_NAME} {__version__}')
 
     group_pex = main_parser.add_argument_group("Parasitic Extraction")
-    group_pex.add_argument("--tech", "-t", dest="tech_pbjson_path", default=None,
+    group_pex.add_argument("--tech", "-t", dest="tech_pbjson_path", required=True,
                            help="Technology Protocol Buffer path (*.pb.json)")
-    group_pex.add_argument("--out_dir", "-o", dest="output_dir_path", default=None,
-                           help="Output directory path")
-    group_pex.add_argument("--lvsdb", "-l", dest="lvsdb_path", default=None, help="KLayout LVSDB path")
+    group_pex.add_argument("--lvsdb", "-l", dest="lvsdb_path", required=True, help="KLayout LVSDB path")
     group_pex.add_argument("--cell", "-c", dest="cell_name", default="TOP", help="Cell (default is TOP)")
+    group_pex.add_argument("--out_dir", "-o", dest="output_dir_path", default=".",
+                           help="Output directory path")
 
     if arg_list is None:
         arg_list = sys.argv[1:]
@@ -42,57 +50,54 @@ def parse_args(arg_list: List[str] = None) -> argparse.Namespace:
 
 
 def validate_args(args: argparse.Namespace):
-    if not args.tech_pbjson_path:
+    found_errors = False
+
+    if not os.path.isfile(args.tech_pbjson_path):
+        error(f"Can't read technology file at path {args.tech_pbjson_path}")
+        found_errors = True
+
+    if not os.path.isfile(args.lvsdb_path):
+        error(f"Can't read KLayout LVSDB file at path {args.lvsdb_path}")
+        found_errors = True
+
+    if found_errors:
+        sys.exit(1)
 
 
-
-def main(args: argparse.Namespace):
-    # jsonpb_path = os.path.realpath(
-    #     os.path.join(os.path.dirname(os.path.realpath(__file__)),
-    #                  "..",
-    #                  "build",
-    #                  "sky130A_tech.pb.json")
-    # )
-    # cell = "nmos_diode2"
-    # lvsdb_path = f"{cell}.lvsdb.gz"
-    # gds_path = f"{cell}_l2ndb.gds.gz"
-    #
-    # output_dir_path = os.path.realpath(
-    #     os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
-    # )
+def main():
+    args = parse_args()
+    validate_args(args)
 
     tech_info = TechInfo.from_json(args.tech_pbjson_path)
 
-    # NOTE: can be L2N database for pure extracted information
     lvsdb = kdb.LayoutVsSchematic()
     lvsdb.read(args.lvsdb_path)
 
-    pex_context = KLayoutExtractionContext.prepare_extraction(top_cell=cell, lvsdb=lvsdb)
-    pex_context.target_layout.write(gds_path)
+    pex_context = KLayoutExtractionContext.prepare_extraction(top_cell=args.cell_name, lvsdb=lvsdb)
+    # pex_context.target_layout.write(gds_path)
 
     fastercap_input_builder = FasterCapInputBuilder(
         pex_context=pex_context,
         tech_info=tech_info
     )
-    fastercap_input_content = fastercap_input_builder.build()
+    input_files_by_circuit: List[Tuple[kdb.Circuit, InputFile3D]] = fastercap_input_builder.build()
 
     def provide_fastcap_file(name: str) -> TextIO:
-        if not os.path.isdir(output_dir_path):
-            os.makedirs(output_dir_path, exist_ok=True)
-        path = os.path.join(output_dir_path, name)
+        if not os.path.isdir(args.output_dir_path):
+            os.makedirs(args.output_dir_path, exist_ok=True)
+        path = os.path.join(args.output_dir_path, name)
         textio = open(path, mode="w")
         return textio
 
     writer = FasterCapFileWriter()
-    writer.write_3d_file(input_file=fastercap_input_content,
-                         file_provider=provide_fastcap_file,
-                         sub_file_strategy=FasterCapSubFileStrategy.MULTI_FILE)
+    for circuit, fastercap_input_content in input_files_by_circuit:
+        writer.write_3d_file(input_file=fastercap_input_content,
+                             file_provider=provide_fastcap_file,
+                             sub_file_strategy=FasterCapSubFileStrategy.MULTI_FILE)
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    validate_args(args)
-    main(args)
+    main()
 
 # # NOTE: this only works from within klayout
 # # class Display:
