8b790b0f581ed1332157e475d4cc6d2b02f3b1d7
Martin Köhler <info@martinjankoehler.com>
2024-12-02 17:34:34 +0100

    Fixup Capacitance Matrix

--- kpex/util/capacitance_matrix.py
+++ kpex/util/capacitance_matrix.py
@@ -5,4 +5,15 @@ from dataclasses import dataclass
 @dataclass
 class CapacitanceMatrix:
     conductor_names: List[str]
-    rows: List[List[float]]
+    rows: List[List[float]]   # NOTE: in µm
+
+    def write_csv(self, output_path: str, separator: str = ';'):
+        with open(output_path, "w") as f:
+            header_line = separator.join(self.conductor_names)
+            f.write(header_line)
+            f.write('\n')
+
+            for row in self.rows:
+                row_line = separator.join('%.12g' % row)
+                f.write(row_line)
+                f.write('\n')
