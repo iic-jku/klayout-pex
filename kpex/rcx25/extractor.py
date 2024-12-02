d4179dcca6d7350f53f7a13b1e4d91ac4b88b23e
Martin Köhler <info@martinjankoehler.com>
2024-12-02 17:34:38 +0100

    kpex/2.5D: move ExtractionResult related dataclasses to extraction_results.py

--- kpex/rcx25/extractor.py
+++ kpex/rcx25/extractor.py
@@ -1,14 +1,13 @@
 #! /usr/bin/env python3
+
 import math
 from collections import defaultdict
-from dataclasses import dataclass, field
 from typing import *
 
 import klayout.db as kdb
 import klayout.rdb as rdb
 
-import process_parasitics_pb2
-from ..klayout.lvsdb_extractor import KLayoutExtractionContext, KLayoutExtractedLayerInfo, GDSPair
+from ..klayout.lvsdb_extractor import KLayoutExtractionContext, GDSPair
 from ..log import (
     console,
     debug,
@@ -17,97 +16,13 @@ from ..log import (
     error
 )
 from ..tech_info import TechInfo
-
+from .extraction_results import *
 import process_stack_pb2
 
-
-NetName = str
-LayerName = str
-CellName = str
-
-
-CoupleKey = Tuple[NetName, NetName]
 
 EdgeInterval = Tuple[float, float]
 ChildIndex = int
 EdgeNeighborhood = List[Tuple[EdgeInterval, Dict[ChildIndex, List[kdb.Polygon]]]]
-
-
-@dataclass
-class NodeRegion:
-    layer_name: LayerName
-    net_name: NetName
-    cap_to_gnd: float
-    perimeter: float
-    area: float
-
-
-@dataclass(frozen=True)
-class SidewallKey:
-    layer: LayerName
-    net1: NetName
-    net2: NetName
-
-
-@dataclass
-class SidewallCap:  # see Magic EdgeCap, extractInt.c L444
-    key: SidewallKey
-    cap_value: float   # femto farad
-    distance: float    # distance in µm
-    length: float      # length in µm
-    tech_spec: process_parasitics_pb2.CapacitanceInfo.SidewallCapacitance
-
-
-@dataclass(frozen=True)
-class OverlapKey:
-    layer_top: LayerName
-    net_top: NetName
-    layer_bot: LayerName
-    net_bot: NetName
-
-
-@dataclass
-class OverlapCap:
-    key: OverlapKey
-    cap_value: float  # femto farad
-    shielded_area: float  # in µm^2
-    unshielded_area: float  # in µm^2
-    tech_spec: process_parasitics_pb2.CapacitanceInfo.OverlapCapacitance
-
-
-@dataclass(frozen=True)
-class SideOverlapKey:
-    layer_inside: LayerName
-    net_inside: NetName
-    layer_outside: LayerName
-    net_outside: NetName
-
-    def __repr__(self) -> str:
-        return f"{self.layer_inside}({self.net_inside})-"\
-               f"{self.layer_outside}({self.net_outside})"
-
-
-@dataclass
-class SideOverlapCap:
-    key: SideOverlapKey
-    cap_value: float  # femto farad
-
-    def __str__(self) -> str:
-        return f"(Side Overlap): {self.key} = {round(self.cap_value, 6)}fF"
-
-
-@dataclass
-class CellExtractionResults:
-    cell_name: CellName
-    # node_regions: Dict[NetName, NodeRegion] = field(default_factory=dict)
-    overlap_coupling: Dict[OverlapKey, OverlapCap] = field(default_factory=dict)
-    sidewall_table: Dict[SidewallKey, SidewallCap] = field(default_factory=dict)
-    sideoverlap_table: Dict[SideOverlapKey, SideOverlapCap] = field(default_factory=dict)
-
-
-@dataclass
-class ExtractionResults:
-    cell_extraction_results: Dict[CellName, CellExtractionResults] = field(default_factory=dict)
 
 
 class RCExtractor:
