50e21bd091fc810cdc07a93fe5f0120acbc4bb68
Martin Köhler <info@martinjankoehler.com>
2024-12-02 17:34:36 +0100

    kpex.sh: get rid of hard-coded python3.12

--- kpex.sh
+++ kpex.sh
@@ -4,4 +4,4 @@ DIR=$(dirname -- $(realpath ${BASH_SOURCE}))
 
 export PYTHONPATH=$DIR/:$DIR/build/python:${PYTHONPATH}
 # /usr/bin/env python3 $DIR/kpex/kpex_cli.py $*
-/usr/bin/env python3.12 -m kpex $*
+/usr/bin/env python3 -m kpex $*
