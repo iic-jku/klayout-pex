120d82567c27940c47dc926c21c06527de95b7bf
Martin Köhler <info@martinjankoehler.com>
2024-12-10 19:41:13 +0100

    kpex/CLI: if --version/--help is requested, don't log the arguments passed

--- klayout_pex/kpex_cli.py
+++ klayout_pex/kpex_cli.py
@@ -647,8 +647,12 @@ class KpexCLI:
         return lvsdb
 
     def main(self, argv: List[str]):
-        info("Called with arguments:")
-        info(' '.join(map(shlex.quote, sys.argv)))
+        if '-v' not in argv and \
+           '--version' not in argv and \
+           '-h' not in argv and \
+           '--help' not in argv:
+            info("Called with arguments:")
+            info(' '.join(map(shlex.quote, sys.argv)))
 
         args = self.parse_args(argv[1:])
 
