b4ae0a08ad724ba06537748e1144030249def14b
Martin Köhler <info@martinjankoehler.com>
2025-02-03 21:04:33 +0100

    WIP: sidewall reimpl

--- klayout_pex/rcx25/extractor.py
+++ klayout_pex/rcx25/extractor.py
@@ -25,6 +25,8 @@
 
 import math
 from collections import defaultdict
+from dataclasses import dataclass
+from enum import IntEnum
 from typing import *
 
 import klayout.db as kdb
@@ -40,12 +42,25 @@ from ..log import (
 )
 from ..tech_info import TechInfo
 from .extraction_results import *
+from .extraction_reporter import ExtractionReporter
 import klayout_pex_protobuf.process_stack_pb2 as process_stack_pb2
 
 
 EdgeInterval = Tuple[float, float]
 ChildIndex = int
-EdgeNeighborhood = List[Tuple[EdgeInterval, Dict[ChildIndex, List[kdb.Polygon]]]]
+EdgeNeighborhood = List[Tuple[EdgeInterval, Dict[ChildIndex, List[kdb.PolygonWithProperties]]]]
+
+
+class EdgeNeighborhoodChildKind(IntEnum):
+    UNINITIALIZED = 0
+    SIDEWALL = 1
+    OTHER_LAYER = 2
+
+
+@dataclass
+class EdgeNeighborhoodChild:
+    kind: EdgeNeighborhoodChildKind
+    layer_name: LayerName
 
 
 class RCExtractor:
@@ -82,38 +97,23 @@ class RCExtractor:
 
         # TODO: for now, we always flatten and have only 1 cell
         cell_name = self.pex_context.top_cell.name
-        report = rdb.ReportDatabase(f"PEX {cell_name}")
-        cell_extraction_result = self.extract_cell(cell_name=cell_name, report=report)
-        extraction_results.cell_extraction_results[cell_name] = cell_extraction_result
+        extraction_report = ExtractionReporter(cell_name=cell_name,
+                                               dbu=self.pex_context.dbu)
+        cell_extraction_results = CellExtractionResults(cell_name=cell_name)
 
-        report.save(self.report_path)
+        self.extract_cell(results=cell_extraction_results,
+                          report=extraction_report)
+        extraction_results.cell_extraction_results[cell_name] = cell_extraction_results
+
+        extraction_report.save(self.report_path)
 
         return extraction_results
 
     def extract_cell(self,
-                     cell_name: CellName,
-                     report: rdb.ReportDatabase) -> CellExtractionResults:
+                     results: CellExtractionResults,
+                     report: ExtractionReporter):
         netlist: kdb.Netlist = self.pex_context.lvsdb.netlist()
         dbu = self.pex_context.dbu
-
-        extraction_results = CellExtractionResults(cell_name=cell_name)
-
-        rdb_cell = report.create_cell(cell_name)
-        rdb_cat_common = report.create_category("Common")
-        rdb_cat_sidewall_old = report.create_category("Sidewall (legacy space_check)")
-        rdb_cat_sidewall = report.create_category("Sidewall (EdgeNeighborhoodVisitor)")
-        rdb_cat_overlap = report.create_category("Overlap")
-        rdb_cat_fringe = report.create_category("Fringe / Side Overlap")
-
-        def rdb_output(parent_category: rdb.RdbCategory,
-                       category_name: str,
-                       shapes: kdb.Shapes | kdb.Region | List[kdb.Edge]):
-            rdb_cat = report.create_category(parent_category, category_name)
-            report.create_items(rdb_cell.rdb_id(),  ## TODO: if later hierarchical mode is introduced
-                                rdb_cat.rdb_id(),
-                                kdb.CplxTrans(mag=dbu),
-                                shapes)
-
         # ------------------------------------------------------------------------
 
         layer_by_name: Dict[LayerName, process_stack_pb2.ProcessStackInfo.LayerInfo] = {}
@@ -131,27 +131,100 @@ class RCExtractor:
             gds_pair = self.gds_pair(layer_name)
             canonical_layer_name = self.tech_info.canonical_layer_name_by_gds_pair[gds_pair]
 
-            all_layer_shapes = self.shapes_of_layer(layer_name) or kdb.Region()
+            all_layer_shapes = self.shapes_of_layer(layer_name)
+            if all_layer_shapes is None:
+                continue
+
             all_layer_shapes.enable_properties()
 
             layer_regions_by_name[canonical_layer_name] += all_layer_shapes
             layer_regions_by_name[canonical_layer_name].enable_properties()
             all_region += all_layer_shapes
 
+        # ------------------------------------------------------------------------
+
+        class GeometryRestorer:
+            def __init__(self, transformation: kdb.IMatrix3d):
+                self.transformation = transformation
+
+            def restore_edge_interval(self, edge_interval: EdgeInterval) -> kdb.Edge:
+                return self.transformation * kdb.Edge(kdb.Point(edge_interval[0], 0),
+                                                      kdb.Point(edge_interval[1], 0))
+
+            def restore_edge(self, edge: kdb.Edge) -> kdb.Edge:
+                return self.transformation * edge
+
+            def restore_polygon(self, polygon: kdb.Polygon) -> kdb.Polygon:
+                return self.transformation * polygon
+
+        # ------------------------------------------------------------------------
+
+        def emit_sidewall(layer_name: LayerName,
+                          edge: kdb.EdgeWithProperties,
+                          edge_interval: EdgeInterval,
+                          polygon: kdb.PolygonWithProperties,
+                          geometry_restorer: GeometryRestorer):
+            net1 = edge.property('net')
+            net2 = polygon.property('net')
+            sidewall_cap_spec = self.tech_info.sidewall_cap_by_layer_name[layer_name]
+
+            # TODO!
+
+            # NOTE: this method is always called for a single nearest edge (line), so the
+            #       polygons have 4 points.
+            #       Polygons points are sorted clockwise, so the edge
+            #       that goes from right-to-left is the nearest edge
+            #nearby_opposing_edge = [e for e in nearest_lateral_shape[1].each_edge() if e.d().x < 0][-1]
+            #nearby_opposing_edge_trans = geometry_restorer.restore_edge(edge) * nearby_opposing_edge
+
+            # C = Csidewall * l * t / s
+            # C = Csidewall * l / s
+
+            avg_length = edge_interval[1] - edge_interval[0]
+            avg_distance = min(polygon.bbox().p1.y, polygon.bbox().p2.y)
+
+            # outside_edge = kdb.Edge(polygon.bbox().p1, polygon.bbox().p2)
+            outside_edge = [e for e in polygon.each_edge() if e.d().x < 0][-1]
+
+            length_um = avg_length * dbu
+            distance_um = avg_distance * dbu
+
+            # NOTE: dividing by 2 (like MAGIC this not bidirectional),
+            #       but we count 2 sidewall contributions (one for each side of the cap)
+            cap_femto = ((length_um * sidewall_cap_spec.capacitance)
+                         / (distance_um + sidewall_cap_spec.offset)
+                         / 2.0       # non-bidirectional (half)
+                         / 1000.0)   # aF -> fF
+
+            info(f"(Sidewall) layer {layer_name}: Nets {net1} <-> {net2}: {round(cap_femto, 5)} fF")
+
+            swk = SidewallKey(layer=layer_name, net1=net1, net2=net2)
+            sidewall_cap = SidewallCap(key=swk,
+                                       cap_value=cap_femto,
+                                       distance=distance_um,
+                                       length=length_um,
+                                       tech_spec=sidewall_cap_spec)
+
+            report.output_sidewall(sidewall_cap=sidewall_cap,
+                                   inside_edge=geometry_restorer.restore_edge_interval(edge_interval),
+                                   outside_edge=geometry_restorer.restore_edge(outside_edge))
+
+        def emit_fringe(inside_layer_name: LayerName,
+                        outside_layer_name: LayerName,
+                        edge: kdb.EdgeWithProperties,
+                        edge_interval: EdgeInterval,
+                        polygon: kdb.PolygonWithProperties,
+                        geometry_restorer: GeometryRestorer):
+            pass
 
         # ------------------------------------------------------------------------
 
         class PEXEdgeNeighborhoodVisitor(kdb.EdgeNeighborhoodVisitor):
             def __init__(self,
-                         child_names: List[str],
-                         tech_info: TechInfo,
-                         report_category: rdb.RdbCategory):
-                self.child_names = child_names
-                # NOTE: child_names[0] is the inside net (foreign)
-                #       child_names[1] is the shielded net (between layers)
-                #       child_names[2:] are the outside nets
-                self.tech_info = tech_info
-                self.report_category = report_category
+                         children_description: List[EdgeNeighborhoodChild]):
+                self.children_description = children_description
+                # NOTE: children_description[0] is the inside net (foreign)
+                #       children_description[1:] are the outside nets
 
             def begin_polygon(self,
                               layout: kdb.Layout,
@@ -166,7 +239,7 @@ class RCExtractor:
             def on_edge(self,
                         layout: kdb.Layout,
                         cell: kdb.Cell,
-                        edge: kdb.Edge,
+                        edge: kdb.EdgeWithProperties,
                         neighborhood: EdgeNeighborhood):
                 #
                 # NOTE: this complex operation will automatically rotate every edge to be on the x-axis
@@ -174,28 +247,47 @@ class RCExtractor:
                 #       so we only have to consider the y-axis to get the near and far distances
                 #
 
-                # TODO: consider z-shielding!
+                geometry_restorer = GeometryRestorer(self.to_original_trans(edge))
 
-                for (x1, x2), polygons_by_net in neighborhood:
-                    if not polygons_by_net:
+                for edge_interval, polygons_by_child in neighborhood:
+                    if not polygons_by_child:
                         continue
 
-                    edge_interval_length = x2 - x1
+                    for child_index, polygons in polygons_by_child.items():
+                        cd = self.children_description[child_index]
+
+                        match cd.kind:
+                            case EdgeNeighborhoodChildKind.SIDEWALL:
+                                for polygon in polygons:
+                                    emit_sidewall(layer_name=cd.layer_name,
+                                                  edge=edge,
+                                                  edge_interval=edge_interval,
+                                                  polygon=polygon,
+                                                  geometry_restorer=geometry_restorer)
+
+                            case EdgeNeighborhoodChildKind.OTHER_LAYER:
+                                for polygon in polygons:
+                                    emit_fringe(inside_layer_name=layer_name,
+                                                outside_layer_name=cd.layer_name,
+                                                edge=edge,
+                                                edge_interval=edge_interval,
+                                                polygon=polygon,
+                                                geometry_restorer=geometry_restorer)
+
 
         for layer_name, layer_region in layer_regions_by_name.items():
-            rdb_cat_layer = report.create_category(rdb_cat_common, f"Layer {layer_name}")
-
             other_layer_names = [oln for oln in layer_regions_by_name.keys() if oln != layer_name]
 
             visitor = PEXEdgeNeighborhoodVisitor(
-                child_names=[layer_name, other_layer_names],
-                tech_info=self.tech_info,
-                report_category=rdb_cat_layer
+                children_description=[EdgeNeighborhoodChild(EdgeNeighborhoodChildKind.SIDEWALL, layer_name)] +
+                                     [EdgeNeighborhoodChild(EdgeNeighborhoodChildKind.OTHER_LAYER, name)
+                                      for name in other_layer_names],
             )
 
-            # children = [kdb.CompoundRegionOperationNode.new_secondary(shapes_inside_net),
-            children = [kdb.CompoundRegionOperationNode.new_primary(),
-                        kdb.CompoundRegionOperationNode.new_foreign()]
+            children = [
+                # kdb.CompoundRegionOperationNode.new_primary(),
+                kdb.CompoundRegionOperationNode.new_foreign()
+            ]
             children += [kdb.CompoundRegionOperationNode.new_secondary(layer_regions_by_name[other_layer_name])
                          for other_layer_name in other_layer_names]
 
@@ -213,4 +305,4 @@ class RCExtractor:
 
             layer_region.complex_op(node)
 
-        return extraction_results
+        return results
