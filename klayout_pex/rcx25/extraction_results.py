c008809d0b8ead04a577cb2cdbd63472b18e4652
Martin Jan Köhler <info@martinjankoehler.com>
2025-08-14 17:31:41 +0200

    Added missing newline

--- klayout_pex/rcx25/extraction_results.py
+++ klayout_pex/rcx25/extraction_results.py
@@ -21,6 +21,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # --------------------------------------------------------------------------------
 #
+
 from __future__ import annotations
 from collections import defaultdict
 from dataclasses import dataclass, field
