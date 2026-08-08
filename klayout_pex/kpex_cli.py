a751ffa7520ef4dd660e62e20f9134382f3ce55e
Torleif Skår <16509259+tskaar@users.noreply.github.com>
2026-08-08 15:52:00 +0200

    feat(magic): add 'thresh', 'minres' and 'mindelay'.
    These variables are the ones that is currently controlling
    the extraction engine.

--- klayout_pex/kpex_cli.py
+++ klayout_pex/kpex_cli.py
@@ -253,6 +253,21 @@ class KpexCLI:
                                  type=int, default=100,
                                  help="Threshold (in Ω) for ignored parasitic resistances (default is %(default)s). "
                                       "(MAGIC command: ext2spice rthresh <value>)")
+        group_magic.add_argument("--magic_thresh", dest="magic_thresh",
+                                 type=int, default=None,
+                                 help="Lumped resistance threshold (in mΩ) to trigger network extraction. "
+                                      "If not supplied, MAGIC's default is implicitly used. "
+                                      "(MAGIC command: extresist threshold <value>)")
+        group_magic.add_argument("--magic_minres", dest="magic_minres",
+                                 type=int, default=None,
+                                 help="Threshold (in mΩ) for removing individual resistors during network simplification. "
+                                      "If not supplied, MAGIC's default is implicitly used. "
+                                      "(MAGIC command: extresist minres <value>)")
+        group_magic.add_argument("--magic_mindel", dest="magic_mindel",
+                                 type=int, default=None,
+                                 help="Minimum delay threshold (in ps) to trigger network extraction. "
+                                      "If not supplied, MAGIC's default is implicitly used. "
+                                      "(MAGIC command: extresist mindelay <value>)")
         group_magic.add_argument("--magic_tolerance", dest="magic_tolerance",
                                  type=float, default=1,
                                  help="Set ratio between resistor and device tolerance (default is %(default)s). "
@@ -624,6 +639,9 @@ class KpexCLI:
                              c_threshold=args.magic_cthresh,
                              r_threshold=args.magic_rthresh,
                              tolerance=args.magic_tolerance,
+                             threshold=args.magic_thresh,
+                             min_res=args.magic_minres,
+                             min_delay=args.magic_mindel,
                              halo=args.magic_halo,
                              short_mode=args.magic_short_mode,
                              merge_mode=args.magic_merge_mode)
